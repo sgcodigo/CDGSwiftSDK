66d506f35c7e6dfb1b17adc3adec953d011bef8c
Vic-L <vljc17@gmail.com>
2017-10-18 09:48:59 +0800

    Initial Commit

---
+++ CDGSwiftSDK/CDGSwiftSDK.h
@@ -0,0 +1,19 @@
+//
+//  CDGSwiftSDK.h
+//  CDGSwiftSDK
+//
+//  Created by Vic-L on 18/10/17.
+//  Copyright © 2017 codigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CDGSwiftSDK.
+FOUNDATION_EXPORT double CDGSwiftSDKVersionNumber;
+
+//! Project version string for CDGSwiftSDK.
+FOUNDATION_EXPORT const unsigned char CDGSwiftSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CDGSwiftSDK/PublicHeader.h>
+
+
